c87b3c40429f80749a4b5bc3e3487df816d0bb99
hanyoseob <hanyoseob@kaist.ac.kr>
2020-01-12 16:48:58 +0900

    Update system parameters.

--- demo_parallelbeam_ct.m
+++ demo_parallelbeam_ct.m
@@ -7,26 +7,28 @@ addpath('./util');
 addpath('./matlab');
 addpath('./mex');
 
+%% Select the device to execute the simulation
+% device: computed language to execute the simulation, choise=['matlab', 'clang'; (string)]
 param.device    = 'clang';
 
 %% X-ray CT System parameter
-% dAngle: Measure from 0 until the angle [degree (float)]
-% nView	: # of the views [element (uint)]
-% dView	: Gap between view_(k) - view_(k-1) [degree (float)]
-% DSO   : Distance from the Source to the Object    [mm (float)]
-% DSD 	: Distance from the Source to the Detector  [mm (float)]
+% dAngle: Measure from 0 until the angle [degree; (float)]
+% nView	: # of the views [element; (uint)]
+% dView	: Gap between view_(k) - view_(k-1) [degree; (float)]
+% DSO   : Distance from the Source to the Object    [mm; (float)]
+% DSD 	: Distance from the Source to the Detector  [mm; (float)]
 
-param.dAngle        = 360;  % degree
-param.nView         = 360; 	% # of unit
-param.dView         = param.dAngle/param.nView;  % degree
-param.DSO           = 400 ; % mm
-param.DSD           = 800;  % mm
+param.dAngle        = 360;
+param.nView         = 360;
+param.dView         = param.dAngle/param.nView;
+param.DSO           = 400;
+param.DSD           = 800;
 
 %% X-ray detector parameter
-% dDctY, dDctX              : Detector pitch [mm (float)]
-% nDctY, nDctX            	: Number of detector [element (uint)]
-% dOffsetDctY, dOffsetDctX  : Index of shifted detector [element (float; +, -)]
-% compute_filtering         : Filtering method, choise=['conv', 'fft' (string)]
+% dDctY, dDctX              : Detector pitch [mm; (float)]
+% nDctY, nDctX            	: Number of detector [element; (uint)]
+% dOffsetDctY, dOffsetDctX  : Index of shifted detector [element; (float; +, -)]
+% compute_filtering         : Filtering method, choise=['conv', 'fft'; (string)]
 % ---------------------------------------------------
 % '*DctY' parameters are only used when 3D CT system. 
 % ---------------------------------------------------
@@ -43,9 +45,9 @@ param.dOffsetDctX   = 20;
 param.compute_filtering = 'fft';
 
 %% Object parameter
-% dImgY, dImgX, dImgZ	: Pixel resolution [mm (float)]
-% nImgY, nImgX, nImgZ	: Matrix size of image [element (uint)]s
-% dOffsetImgY, dOffsetImgX, dOffsetImgZ	: Index of shifted image [element (float; +, -)]
+% dImgY, dImgX, dImgZ	: Pixel resolution [mm; (float)]
+% nImgY, nImgX, nImgZ	: Matrix size of image [element; (uint)]s
+% dOffsetImgY, dOffsetImgX, dOffsetImgZ	: Index of shifted image [element; (float; +, -)]
 % ----------------------------------------------------
 % '*ImgZ' parameters are only used when 3D CT system. 
 % ----------------------------------------------------
